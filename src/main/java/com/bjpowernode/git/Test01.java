168adf7136301c5f70425ea9ae65c90c5ebb906d
wgsizz <wgsizz@163.com>
2020-09-07 23:01:02 +0800

    Git测试---04

--- src/main/java/com/bjpowernode/git/Test01.java
+++ src/main/java/com/bjpowernode/git/Test01.java
@@ -10,6 +10,7 @@ public class Test01 {
         System.out.println("Git测试-------01");
         System.out.println("Git测试-------02");
         System.out.println("Git测试-------03");
+        System.out.println("Git测试-------04");
 
     }
 }
